b6f88d190252e5307a0a6c0756271804c8f453f1
Kh3m <khem6333@gmail.com>
2020-11-11 11:28:57 +0100

    fix: migration error

--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,17 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      // provider: function() {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      // },
+      // network_id: '*',
+      // gas: 6721975
+
+      port: 7545, // Ganache UI
+      host: "127.0.0.1", 
+      network_id: "*", 
+      gas: 6721975
+      
     }
   },
   compilers: {
